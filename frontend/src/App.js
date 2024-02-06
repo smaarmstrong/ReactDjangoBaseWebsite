38e4904ccfdf93130be9da23f9242a8c54ad4651
Sean Armstrong <seanmaarmstrong@gmail.com>
2024-02-05 20:06:27 -0800

    Update Website Style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage/HomePage";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <Header />
+      <Container>
+        <Routes>
+          <Route path="/" element={<HomePage />} exact />
+        </Routes>
+      </Container>
       <Footer />
     </Router>
   );
