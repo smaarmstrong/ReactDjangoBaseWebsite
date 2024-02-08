70dcc35972eeb04c24102b81d2255e41a7480273
Sean Armstrong <seanmaarmstrong@gmail.com>
2024-02-08 12:22:44 -0800

    Create login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage/HomePage";
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Container>
         <Routes>
           <Route path="/" element={<HomePage />} exact />
+          <Route path="/login" element={<LoginPage />} exact />
         </Routes>
       </Container>
       <Footer />
